7a92105767362bba107f3ab5d5666d66fa5a19d7
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2021-12-11 23:40:37 +1100

    program simplified using Methods

--- src/main.rb
+++ src/main.rb
@@ -1,35 +1,13 @@
-puts " "
-print "\t\tWelcome to BUDGET SOLUTIONS for Professional & Affordable Solutions.\n\n"
+require_relative 'methods'
 
-print "\t\tTo let us help you create the very best BEST budget for you,\n\n"
+welcome_msg
 
 print "\t\tIs it OK if we ask you a few Questions(Y/N)?\n\n"
 
 response = (gets.chomp).capitalize
 
-case response
-when "Y" 
-    puts "\t\tGreat. We will first ask you a few quesitons on your income"
-when "N" 
-    puts "\t\tNo Worries. Don't hesitate to return if you feel differently"
-else
-    while ((response != "Y")&&(response != "N")) do
-        puts "\t\tPlease Enter Y/N"
-        response = (gets.chomp).capitalize
-        puts response
-    end
-        if (response == "Y") then
-            puts "\t\tGreat. We will first ask you a few quesitons on your income"
-        else
-            puts "\t\tNo Worries. Don't hesitate to return if you feel differently"
-            exit
-        end    
-    
-end
+questions(response)
+  
+budget_calculator
 
-puts "How much is your personal income?"
-income = (gets.chomp).to_i
-puts "How much is your personal Expense?"
-expense = (gets.chomp).to_i
-puts "Now your budget surplus/deficit is:"
-puts "#{income-expense}"
+   
---
+++ src/methods.rb
@@ -0,0 +1,52 @@
+def welcome_msg
+    puts " "
+    print "\t\tWelcome to BUDGET SOLUTIONS for Professional & Affordable Solutions.\n\n"
+
+    print "\t\tTo let us help you create the very best BEST budget for you,\n\n"
+end
+
+def questions(response)
+    while ((response != "Y")&&(response != "N")) do
+        puts "\t\tPlease Enter Y/N"
+        response = (gets.chomp).capitalize
+    end
+    case response
+        when "Y" 
+            puts "\t\tGreat. We will first ask you a few quesitons on your income"
+        when "N" 
+            puts "\t\tNo Worries. Don't hesitate to return if you feel differently"
+            exit
+    end
+      
+end  
+
+
+    def budget_calculator
+
+        puts "How much is your personal income?"
+        
+        income = test_integer(gets.chomp)
+       puts "How much is your personal Expense?"
+       
+        expense = test_integer(gets.chomp)
+        puts "Now your budget surplus/deficit is:"
+        puts "#{income-expense}"
+
+    end
+
+
+def test_integer(test_num)
+
+    num_cond = Integer(test_num) rescue false
+    if num_cond
+        return test_num.to_i
+    else
+        while num_cond == false do
+            puts "Please enter a value closest to an integer"
+            num_cond = Integer(gets.chomp) rescue false
+        end
+        return num_cond.to_i
+    end
+
+end    
+
